7ef2656bb73a08ab6e7bda2fedea8725e62554d3
Felix Viola <kontakt@felixviola.de>
2023-09-23 21:11:50 +0200

    update2
    Former-commit-id: b460a4280ac09b16c3a1c8f51cebad86518956e4

---
+++ api/wakeUpNow.php
@@ -0,0 +1,13 @@
+<?php
+
+$globals["database_name"] = "j_tasks";
+require_once("../includes/db_connection.php");
+
+$db = $globals['db'];
+
+$sql = "INSERT INTO `wakeup_times` (`ID`, `date`, `time`, `wake_up`) VALUES (NULL, CURRENT_DATE, CURRENT_TIME, '1');";
+mysqli_query($db, $sql);
+
+
+
+?>
--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,18 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "***REMOVED***";
-$password = "***REMOVED***";
+$password = "";
 $database = $globals["database_name"];
 
 $globals['db'] = mysqli_connect($servername, $username, $password, $database);
-
-// $servername = "192.168.2.122";
-// $username = "***REMOVED***";
-// $password = "***REMOVED***";
-// $database = "j_tasks"; #$globals["database_name"];
-//
-// $globals['db'] = mysqli_connect($servername, $username, $password);
-
 
 if(!$globals['db']) {
   die("Connection failed: ".mysqli_connect_error());
