5f1315c88fd6f8bbc6c97efce738c4237468021e
Felix Viola <kontakt@felixviola.de>
2023-10-03 08:54:35 +0200

    updates

--- api/createTask.php
+++ api/createTask.php
@@ -11,14 +11,14 @@ if(isset($body->title)) {
   $name = $body->title;
   $desc = $body->description;
   $duedate  = $body->due;
-  $duetime  = $body->due_time != "" ? $body->due_time: "NULL";
-  $duration  = $body->duration != "" ? $body->duration: "NULL";
+  $duetime  = $body->due_time != "" ? "'".$body->due_time."'" : "NULL";
+  $duration  = $body->duration != "" ? "'".$body->duration."'" : "NULL";
   $priority  = $body->priority;
   $category  = $body->category;
   $location  = $body->location;
 
   $sql = "INSERT INTO `tasks` (`ID`, `Name`, `description`, `due`, `due_time`, `done`, `duration`, `priority`, `category`, `location`) 
-                      VALUES (NULL, '$name', '$desc', '$duedate', '$duetime', NULL, '$duration', '$priority', '$category', '$location');";
+                      VALUES (NULL, '$name', '$desc', '$duedate', $duetime, NULL, $duration, '$priority', '$category', '$location');";
   // echo $sql; exit();
   mysqli_query($db, $sql);
   $res = mysqli_query($db, "SELECT LAST_INSERT_ID() as ID;");
