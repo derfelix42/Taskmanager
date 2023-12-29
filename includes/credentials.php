f72a9874163ddc0dc309ebc9ec6129bb356c5743
Felix Viola <kontakt@felixviola.de>
2023-12-29 12:30:08 +0100

    Use a dotenv file for credentials

---
+++ includes/credentials.php
@@ -0,0 +1,21 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT']."/Taskmanager/includes/dotenv.php");
+(new DotEnv(__DIR__ . '/../.env'))->load();
+
+$username = getenv('MYSQL_USERNAME');
+$password = getenv('MYSQL_PASSWORD');
+
+# Weather API
+$apiKey = getenv('WEATHER_API_KEY');
+$samplesApiKey = getenv('WEATHER_SAMPLE_API_KEY');
+$cityId = getenv('WEATHER_CITY_ID');
+
+# Bahn API
+$first_station_id = getenv('BAHN_API_FROM_ID');
+$second_station_id = getenv('BAHN_API_TO_ID');
+
+# StayFree - UUID
+$SF_uuid = getenv('STAY_FREE_UUID');
+
+
+?>
