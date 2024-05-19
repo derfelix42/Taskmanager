725c66de20e8e87e357ad820fc5787675b7bf25c
Felix Viola <felix.viola@uni-bonn.de>
2024-05-19 15:50:17 +0200

    improve rpi setup script

--- documentation/rpi-setup.sh
+++ documentation/rpi-setup.sh
@@ -18,8 +18,12 @@ git clone https://github.com/derfelix42/Taskmanager.git
 cd Taskmanager
 git checkout 5-docker-deploy
 
+# create .env file
+cp .env.sample .env
+
 # Run docker compose up
 echo "Starting up the software..."
-sudo docker-compose up -d
+sudo docker-compose up -d --build
 
-echo "Installation complete!"
+echo "Installation complete! Visit this Pi in the browser on port 80 on the correct IP address"
+ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
