bc7fd6bf1a45b1a3d2f3b7f3b02e2ba179883721
Felix Viola <felix.viola@uni-bonn.de>
2024-07-16 19:54:34 +0200

    improve sleep history sql query

--- www/api/getSleepHistory.php
+++ www/api/getSleepHistory.php
@@ -9,7 +9,7 @@ $db = $globals['db'];
 if(isset($_GET['date'])) {
   $date = $_GET['date'];
 
-  $sql = "SELECT start_time, stop_time, TIMESTAMPDIFF( SECOND, start_time, stop_time ) AS sleep_secs, SEC_TO_TIME(TIMESTAMPDIFF(SECOND, start_time, stop_time)) AS sleep_hours FROM `sleep_history` WHERE WEEK(start_time, 1) = WEEK('$date', 1) AND YEAR(start_time) = YEAR('$date') ORDER BY start_time ASC;";
+  $sql = "SELECT start_time, stop_time, DAYOFWEEK(start_time) as sleep_dow, DAYOFWEEK(stop_time) as wakeup_dow, TIME(start_time) as sleep_time, TIME(stop_time) as wakeup_time, TIMESTAMPDIFF( SECOND, start_time, stop_time ) AS sleep_secs, SEC_TO_TIME(TIMESTAMPDIFF(SECOND, start_time, stop_time)) AS sleep_hours FROM `sleep_history` WHERE (WEEK(start_time, 1) = WEEK('$date', 1) OR WEEK(stop_time, 1) = WEEK('$date', 1)) AND (YEAR(start_time) = YEAR('$date') OR YEAR(stop_time) = YEAR('$date')) ORDER BY start_time ASC;";
   $result = mysqli_query($db, $sql);
 
 
