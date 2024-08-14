4f2f4ef2d5fdd69e4dde63a9afe3c2bd976ddcd8
Felix Viola <felix.viola@uni-bonn.de>
2024-08-14 18:08:12 +0200

    add is_active flag to sleep history api request

--- www/api/getSleepHistory.php
+++ www/api/getSleepHistory.php
@@ -9,7 +9,7 @@ $db = $globals['db'];
 if(isset($_GET['date'])) {
   $date = $_GET['date'];
 
-  $sql = "SELECT start_time, IFNULL(stop_time, NOW()) as stop_time, DAYOFWEEK(start_time) as sleep_dow, DAYOFWEEK(IFNULL(stop_time, NOW())) as wakeup_dow, TIME(start_time) as sleep_time, TIME(IFNULL(stop_time, NOW())) as wakeup_time, TIMESTAMPDIFF( SECOND, start_time, IFNULL(stop_time, NOW()) ) AS sleep_secs, SEC_TO_TIME(TIMESTAMPDIFF(SECOND, start_time, IFNULL(stop_time, NOW()))) AS sleep_hours FROM `sleep_history` WHERE (WEEK(start_time, 1) = WEEK('$date', 1) OR WEEK(IFNULL(stop_time, NOW()), 1) = WEEK('$date', 1)) AND (YEAR(start_time) = YEAR('$date') OR YEAR(IFNULL(stop_time, NOW())) = YEAR('$date')) ORDER BY start_time ASC;";
+  $sql = "SELECT start_time, IFNULL(stop_time, NOW()) as stop_time, stop_time IS NULL AS is_active,  DAYOFWEEK(start_time) as sleep_dow, DAYOFWEEK(IFNULL(stop_time, NOW())) as wakeup_dow, TIME(start_time) as sleep_time, TIME(IFNULL(stop_time, NOW())) as wakeup_time, TIMESTAMPDIFF( SECOND, start_time, IFNULL(stop_time, NOW()) ) AS sleep_secs, SEC_TO_TIME(TIMESTAMPDIFF(SECOND, start_time, IFNULL(stop_time, NOW()))) AS sleep_hours FROM `sleep_history` WHERE (WEEK(start_time, 1) = WEEK('$date', 1) OR WEEK(IFNULL(stop_time, NOW()), 1) = WEEK('$date', 1)) AND (YEAR(start_time) = YEAR('$date') OR YEAR(IFNULL(stop_time, NOW())) = YEAR('$date')) ORDER BY start_time ASC;";
   $result = mysqli_query($db, $sql);
 
 
