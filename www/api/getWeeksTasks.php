d96eb4f98f47b83df87b964fca66ad924cad6abe
Felix Viola <felix.viola@uni-bonn.de>
2025-01-02 17:30:05 +0100

    replace individual YEAR,WEEK with YEARWEEK to fix issue

--- www/api/getWeeksTasks.php
+++ www/api/getWeeksTasks.php
@@ -17,12 +17,12 @@ if(isset($_GET['date'])) {
             WHERE  WEEK(due,1) = WEEK('$date',1)
             ORDER BY due ASC, due_time ASC, priority DESC, category, Name";
 
-            /* History + Future */
+  /* History + Future */
   $sql = "(SELECT tasks.ID, location, Name, category, description, DATE(start_time) as due, TIME(start_time) as due_time, DAYOFWEEK(due) AS DOW, duration, TIMESTAMPDIFF(DAY, NOW(), due) AS daysLeft, if('$date'>due, 11, priority) as priority, color, DAYOFWEEK(due) as dayofweek, done, TIMESTAMPDIFF(SECOND, start_time, IFNULL(stop_time, CURRENT_TIMESTAMP)) as time_spent
             FROM `task_history`
             JOIN tasks on taskID = tasks.ID
             JOIN category ON tasks.category = category.ID
-            WHERE WEEK(start_time,1) = WEEK('$date',1) AND YEAR(start_time) = YEAR('$date') AND deleted = 0
+            WHERE YEARWEEK(start_time,1) = YEARWEEK('$date', 1) AND deleted = 0
             ORDER BY due ASC, due_time ASC, priority DESC, category, Name)
           UNION
           (SELECT tasks.ID, location, Name, category, description, due, IF(due=CURRENT_DATE, IF(due_time < CURRENT_TIME AND due_time+duration >CURRENT_TIME, CURRENT_TIME, due_time), due_time) as due_time,
@@ -30,7 +30,7 @@ if(isset($_GET['date'])) {
             FROM `tasks`
             JOIN category ON tasks.category = category.ID
             LEFT JOIN (SELECT taskID, SUM(TIMESTAMPDIFF(SECOND, start_time, IFNULL(stop_time, CURRENT_TIMESTAMP))) as time_spent_new FROM `task_history` GROUP BY taskID) as b ON tasks.ID = b.taskID
-            WHERE  (WEEK(due,1) = WEEK('$date',1)) AND YEAR(due) = YEAR('$date') AND ((due > CURRENT_DATE) OR (due = CURRENT_DATE AND due_time+duration > CURRENT_TIME)) AND deleted = 0 AND done IS NULL
+            WHERE  (YEARWEEK(due,1) = YEARWEEK('$date', 1)) AND ((due > CURRENT_DATE) OR (due = CURRENT_DATE AND due_time+duration > CURRENT_TIME)) AND deleted = 0 AND done IS NULL
             ORDER BY due ASC, due_time ASC, priority DESC, category, Name)";
   $result = mysqli_query($db, $sql);
 
