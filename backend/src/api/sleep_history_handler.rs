cafb5e4c4fe6dbfa5cb0c525f7d9ffd0e4568092
Felix Viola <felix.viola@uni-bonn.de>
2025-11-09 23:00:31 +0100

    add endpoint to get week specific sleep history

--- backend/src/api/sleep_history_handler.rs
+++ backend/src/api/sleep_history_handler.rs
@@ -4,7 +4,9 @@ use serde::Deserialize;
 use crate::{database::Db, models::SleepHistoryEntry};
 
 pub fn sleep_history_router() -> Router<Db> {
-    Router::new().route("/", get(get_sleep_history))
+    Router::new()
+        .route("/", get(get_sleep_history))
+        .route("/week", get(get_sleep_history_week))
 }
 
 #[derive(Deserialize)]
@@ -21,6 +23,21 @@ pub async fn get_sleep_history(
         .get_sleep_history_page(payload.limit, payload.offset)
         .await
     {
+        Ok(history) => Json(history),
+        Err(_) => Json(vec![]),
+    }
+}
+
+#[derive(Deserialize)]
+pub struct SleepHistoryWeekParam {
+    pub week: String,
+}
+
+pub async fn get_sleep_history_week(
+    State(database): State<Db>,
+    Json(payload): Json<SleepHistoryWeekParam>,
+) -> Json<Vec<SleepHistoryEntry>> {
+    match database.get_sleep_history_for_week(&payload.week).await {
         Ok(history) => Json(history),
         Err(_) => Json(vec![]),
     }
