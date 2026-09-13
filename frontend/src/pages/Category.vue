c27dc278a57e7c6c17de8f3c5ccc976519fd000b
Felix Viola <felix.viola@uni-bonn.de>
2026-09-13 23:13:00 +0200

    starting first functionality for Category View

--- frontend/src/pages/Category.vue
+++ frontend/src/pages/Category.vue
@@ -1,7 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getCategoryColors } from '@/api/api';
+import { computed, onMounted, reactive, watch } from 'vue';
+
+let categories = reactive({})
+
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const categoryInfo = computed(() => {
+  let id = route.params.id
+
+  if (Array.isArray(id)) {
+    id = id[0]
+  }
+
+  // Route params are normally strings; object keys work as strings too.
+  return categories[id] ?? null
+})
+
+
+onMounted(async () => {
+    let cats = await getCategoryColors();
+    console.log(cats)
+    Object.assign(categories, cats)
+})
+
+
+</script>
 
 <template>
-    <h2>Category {{ $route.params.id }} {{ $route.params.prefix }}</h2>
+    <h2>Category "{{categoryInfo.Bezeichnung }}" {{ $route.params.prefix }}</h2>
+    <!-- <pre>{{ JSON.stringify(categories, null, 2) }}</pre> -->
 </template>
 
 <style></style>
